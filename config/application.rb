ff14b4c5727e69b38a90cbf5645caf9b5e408c99
SteamShon <shom83@gmail.com>
2012-08-21 02:35:42 +0900

    fix faye server

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,5 @@ module Classdojo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = false
   end
 end
