04e13927d5dfb6f3560d2a1616c6a807afcb23c4
SteamShon <shom83@gmail.com>
2012-08-21 01:38:13 +0900

    fix precompile option application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,7 +23,7 @@ module ApplicationHelper
   
   def broadcast(channel, &block)
     message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-    uri = URI.parse("http://localhost:9292/faye")
+    uri = URI.parse("teacher-note.herokuapp.com/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 
