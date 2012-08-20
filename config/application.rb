40654a545ff3570bace67a6f33b4e9cdcae28fc6
SteamShon <shom83@gmail.com>
2012-08-21 01:31:54 +0900

    fix precompile option application

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "net/http"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(*Rails.groups(:assets => %w(development test production)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
