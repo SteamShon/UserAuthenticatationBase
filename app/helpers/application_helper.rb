86d50b285583b4ddb65a4d0f6c0a75ea1cb42f19
SteamShon <shom83@gmail.com>
2012-08-21 01:51:17 +0900

    fix faye server

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,7 +23,7 @@ module ApplicationHelper
   
   def broadcast(channel, &block)
     message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-    uri = URI.parse("http://teacher-note.herokuapp.com/faye")
+    uri = URI.parse("http://fayeserverexample.herokuapp.com/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 
